9164ae76f9c3637cdea4f8677b8b609347523fb2
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-10-25 21:33:11 +0300

    setdefault for create_user & create_superuser

--- authuser/models.py
+++ authuser/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.models import UserManager
 from django.db import models
 
@@ -12,3 +13,13 @@ class CustomUserManager(UserManager):
         user.save(using=self._db)
 
         return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email=None, password=None, **extra_fields): 
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)

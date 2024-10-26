316bd8983dcbadd85d9db3fc4c4564f890f59ec0
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-10-26 20:44:21 +0300

    URL Configuration

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import Home
 
 urlpatterns = [
-    
-
+    path('', Home.as_view()),
 
 ]

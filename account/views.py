9973a241a27e4c2f8b92c217f22a4c7b5272f1f5
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-10-26 22:45:21 +0300

    Create CRUD Views for User

--- account/views.py
+++ account/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+
 class Home(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -10,3 +14,13 @@ class Home(APIView):
     def get(self, request):
         content = {'message': 'Hello, World!'}
         return Response(content)
+
+# List and Create Users
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Retrieve, Update, and Delete a User by ID
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

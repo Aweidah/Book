c873f182c9874110d11345de0512daaed9c1742a
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-10-26 20:39:23 +0300

    Create API Views

--- main/views.py
+++ main/views.py
@@ -2,6 +2,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .models import *
 from .serializers import *
 
@@ -13,3 +16,12 @@ def test(request):
         return Response(serializer.data, status=200)
     elif request.method == "POST":
         return Response(serializer.errors, status=400)
+    
+
+class Home(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)

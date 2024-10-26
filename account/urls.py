9854c4fa297fcfb7b6a7757d7b9a1eab604922d8
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-10-26 22:47:08 +0300

    Set Up the URLs

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from .views import Home
+from .views import UserListCreateView, UserRetrieveUpdateDestroyView
+
 
 urlpatterns = [
     path('', Home.as_view()),
+
+    path('users/', UserListCreateView.as_view(), name='user-list-create'),                 # GET for list, POST for create
+    path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),  # GET, PUT, DELETE for specific user
 
 ]

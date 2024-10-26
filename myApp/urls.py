64de37709ae4330f13f5135ca106278c7c80aa07
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-10-26 21:20:33 +0300

    .

--- myApp/urls.py
+++ myApp/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
-    path('', include('myapp.urls')), # include your app urls.py here
+    path('', include('myApp.urls')), # include your app urls.py here
 
 
 ]

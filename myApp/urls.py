4c5ba62f7be6fa8077c995381d555e2805300d36
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-10-26 20:36:42 +0300

    Token Endpoints

--- myApp/urls.py
+++ myApp/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 
@@ -22,6 +23,9 @@ urlpatterns = [
     path('main/', include('main.urls')),
     path('account/', include('account.urls')),
     
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
+    path('', include('myapp.urls')), # include your app urls.py here
 
 
 ]

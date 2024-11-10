99b85f59617988254fba21a126e5eb740350ef5f
Aweidah <108875224+Aweidah@users.noreply.github.com>
2024-11-10 23:35:13 +0300

    .

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Home
 from .views import UserListCreateView, UserRetrieveUpdateDestroyView
 
 from rest_framework.routers import DefaultRouter
@@ -10,7 +9,6 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', Home.as_view()),
 
     path('get_users/', UserListCreateView.as_view(), name='user-list-create'),                 # GET for list, POST for create
     path('get_users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),  # GET, PUT, DELETE for specific user
--- account/views.py
+++ account/views.py
@@ -1,4 +1,3 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -15,12 +14,7 @@ from rest_framework.decorators import action
 # Token Generation
 from rest_framework_simplejwt.tokens import RefreshToken
 
-class Home(APIView):
-    permission_classes = [IsAuthenticated]
 
-    def get(self, request):
-        content = {'message': 'Hello, World!'}
-        return Response(content)
 
 # List and Create Users
 class UserListCreateView(generics.ListCreateAPIView):
